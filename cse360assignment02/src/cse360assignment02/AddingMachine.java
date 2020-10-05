41ca169e088f567969781c0a950993d931f68b4f
Sai <svadlam8@asu.edu>
2020-10-05 01:38:01 -0700

    Initial check in of CSE360 Assignmetn2

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,33 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	// declaring variable
+	private int total;
+  
+	// constructor that initializes variable
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+  
+	// method that returns the total
+	public int getTotal () {
+		return 0;
+	}
+  
+	// method that adds the parameter "value" to the total
+	public void add (int value) {
+	}
+
+	// method that subtracts the parameter "value" from the total
+	public void subtract (int value) {
+	}
+
+	// prints the output in the required formatting
+	public String toString () {
+		return "";
+	}
+
+	// clears value
+	public void clear() {
+	}
+}
